d963a38c66377b5b4d0005184f631540ff125d38
13inuo0 <13inu.o0@gmail.com>
2026-05-07 16:34:00 +0900

    함수 오버로드2

--- overLoad_ex.ts
+++ overLoad_ex.ts
@@ -10,3 +10,19 @@ function greet3(name: string, age?: number): string {
 
 console.log(greet3("Alice"));
 console.log(greet3("Bob", 30));
+
+// 시그니처
+function add4(n1: number, n2: number): number;
+function add4(n1: string, n2: string): string;
+// 실제 구현
+function add4(n1: number | string, n2: number | string): number | string {
+  if (typeof n1 === "number" && n2 === "number") return n1 + n2;
+  else if (typeof n1 === "string" && n2 === "string") return n1 + n2;
+  throw new Error("Inavalid arguments");
+}
+
+const res1 = add4("a", "b"); //문자열 반환
+const res2 = add4(10, 20); //덧셈 연산
+
+console.log(res1);
+console.log(res2);
